a883d76a2938d800fda550d4f9b0430723f10c11
isklikas <jsklikas@gmail.com>
2017-03-16 12:31:57 +0200

    Somehow, I forgot to commit the new files

---
+++ notecreator/NotePrefManager.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+@interface NotePrefManager: NSObject{}
+
+- (BOOL)titleEnabled;
+- (BOOL)autoKeyboard;
+- (BOOL)saveOnCancel;
+
+@end
---
+++ notecreator/NotePrefManager.m
@@ -0,0 +1,29 @@
+#import "NotePrefManager.h"
+
+@interface NotePrefManager()
+@end
+
+@implementation NotePrefManager
+
+- (BOOL)titleEnabled {
+	  BOOL titleEnabled = [[[self preferencesDictionary] objectForKey:@"SwitchCell6"] boolValue];
+		return titleEnabled;
+}
+
+- (BOOL)autoKeyboard {
+	  BOOL autoKeyboard = [[[self preferencesDictionary] objectForKey:@"SwitchCell5"] boolValue];
+		return autoKeyboard;
+}
+
+- (BOOL)saveOnCancel {
+	  BOOL saveOnCancel = [[[self preferencesDictionary] objectForKey:@"SwitchCell2"] boolValue];
+		return saveOnCancel;
+}
+
+- (NSDictionary *)preferencesDictionary {
+	  NSString *path = [@"/var/mobile/Library/Preferences" stringByAppendingPathComponent:@"com.isklikas.NoteCreatorPrefs.plist"];
+    NSDictionary *dictionary = [[NSDictionary alloc] initWithContentsOfFile:path];
+		return dictionary;
+}
+
+@end
